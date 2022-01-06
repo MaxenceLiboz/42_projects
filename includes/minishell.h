7a6821bd75a6bb60c0f9e174c5ad8c5ee2ee04d4
mliboz <maxoulib@gmail.com>
2022-01-06 14:21:13 +0100

    Add freestr

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mliboz <mliboz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 13:37:52 by mliboz            #+#    #+#             */
-/*   Updated: 2022/01/05 09:27:27 by mliboz           ###   ########.fr       */
+/*   Updated: 2022/01/06 13:00:04 by mliboz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,22 @@
 # define BLUE "\033[0;34m"
 # define END_COLOR "\033[0m"
 
+typedef struct s_string
+{
+	char	*str;
+	int		size;
+	int		max_size;
+}	t_string;
+
+typedef struct s_array_string
+{
+	void	*t_string;
+	int		size;
+	int		max_size;
+}	t_array_string;
+
 char	*create_prompt(char *color, char *str);
 int		exec_builtin(char **str);
+void	freestr(char *str);
 
 #endif
