6e5c9ce830ebf00805e74130c904d008d03d67e7
mliboz <maxoulib@gmail.com>
2022-01-10 12:58:05 +0100

    add reinit_array_string

--- srcs/t_array_string/array_string_functions.c
+++ srcs/t_array_string/array_string_functions.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   array_string_functions.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maxenceliboz <maxenceliboz@student.42.f    +#+  +:+       +#+        */
+/*   By: mliboz <mliboz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 11:37:42 by mliboz            #+#    #+#             */
-/*   Updated: 2022/01/08 15:25:16 by maxencelibo      ###   ########.fr       */
+/*   Updated: 2022/01/10 12:56:45 by mliboz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,21 @@ void	init_array_string(t_array_string *array_string, int size, int to_malloc)
 	array_string->array = malloc(sizeof(t_string) * size);
 	if (!array_string->array)
 		exit(-1);
+}
+
+void	reinit_array_string(t_array_string *array)
+{
+	int		i;
+	int		y;
+
+	i = 0;
+	y = 0;
+	if (!array->array)
+		return ;
+	while (y < array->max_size)
+		reinit_string(&array->array[y++]);
+	free(array->array);
+	reinit_string(&array->command);
+	array->max_size = 0;
+	array->size = 0;
 }
