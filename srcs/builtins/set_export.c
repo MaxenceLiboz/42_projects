11fed62161fbecce8c677fff4bd7b950156931c1
mliboz <maxoulib@gmail.com>
2022-01-11 13:34:32 +0100

    new sorted

--- srcs/builtins/set_export.c
+++ srcs/builtins/set_export.c
@@ -3,41 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   set_export.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tarchimb <tarchimb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mliboz <mliboz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 09:22:24 by tarchimb          #+#    #+#             */
-/*   Updated: 2022/01/10 18:36:27 by tarchimb         ###   ########.fr       */
+/*   Updated: 2022/01/11 09:57:36 by mliboz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-char	*find_arg(t_lst_env *env, char *str)
-{
-	t_lst_env	*tmp;
+// static void	set_struct(t_swap *swap, t_lst_env **env)
+// {
+// 	swap->last = NULL;
+// 	swap->head = *env;
+// 	swap->next = swap->head->next;
+// }	
 
-	tmp = env;
-	while (tmp->next)
-	{
-		if (ft_strncmp(tmp->name_var.str, str,
-				ft_strlen(tmp->name_var.str)) == 0)
-			return (tmp->var.str);
-		tmp = tmp->next;
-	}
-	return (NULL);
-}
+// void	ascii_ordering(t_lst_env **env)
+// {
+// 	t_swap	swap;
+
+// 	set_struct(&swap, env);
+// 	while (swap.next)
+// 	{
+// 		if (ft_strncmp(swap.head->name_var.str, swap.next->name_var.str,
+// 				swap.head->name_var.size) > 0)
+// 		{
+// 			swap.head->next = swap.next->next;
+// 			swap.next->next = swap.head;
+// 			if (swap.last)
+// 				swap.last->next = swap.next;
+// 			else
+// 				*env = swap.next;
+// 			set_struct(&swap, env);
+// 		}
+// 		else
+// 		{
+// 				swap.last = swap.head;
+// 				swap.head = swap.last->next;
+// 				swap.next = swap.head->next;
+// 		}
+// 	}
+// }
 
 //In this function, need to set var.type to ENV if the name_var contain '='
 //As well, the substr function isn't working if the = is not found
-static int	init_lst_str(char *envp, t_lst_env *new_export, t_lst_env *new_env)
+static int	head_env_init(char *envp, t_lst_env *new_export,
+			t_lst_env *new_env)
 {
 	int	i;
 
 	i = 0;
-	init_string(&new_export->name_var, "", TRUE);
-	init_string(&new_env->name_var, "", TRUE);
-	init_string(&new_export->var, "", TRUE);
-	init_string(&new_env->var, "", TRUE);
 	while (envp[i] != '=' && envp[i])
 		i++;
 	sub_string(&new_export->name_var, envp, 0, i);
@@ -45,40 +61,6 @@ static int	init_lst_str(char *envp, t_lst_env *new_export, t_lst_env *new_env)
 	sub_string(&new_env->var, envp, i + 1, (ft_strlen(envp) - i) + 1);
 	sub_string(&new_export->var, envp, i + 1, (ft_strlen(envp) - i) + 1);
 	return (1);
-}
-
-static void	set_struct(t_swap *swap, t_lst_env **env)
-{
-	swap->last = NULL;
-	swap->head = *env;
-	swap->next = swap->head->next;
-}	
-
-void	ascii_ordering(t_lst_env **env)
-{
-	t_swap	swap;
-
-	set_struct(&swap, env);
-	while (swap.next)
-	{
-		if (ft_strncmp(swap.head->name_var.str, swap.next->name_var.str,
-				swap.head->name_var.size) > 0)
-		{
-			swap.head->next = swap.next->next;
-			swap.next->next = swap.head;
-			if (swap.last)
-				swap.last->next = swap.next;
-			else
-				*env = swap.next;
-			set_struct(&swap, env);
-		}
-		else
-		{
-				swap.last = swap.head;
-				swap.head = swap.last->next;
-				swap.next = swap.head->next;
-		}
-	}
 }
 
 //Should we do use add_back or add_front
@@ -91,14 +73,14 @@ int	set_export(char **envp, t_head_env *head)
 	i = 0;
 	while (envp[i])
 	{
-		new_export = ft_lstnew(0);
-		new_env = ft_lstnew(0);
-		ft_lstadd_back(&head->export, new_export);
-		ft_lstadd_back(&head->env, new_env);
-		if (!init_lst_str(envp[i], new_export, new_env))
+		new_export = lst_env_new("", "");
+		new_env = lst_env_new("", "");
+		lst_env_add_back(&head->export, new_export);
+		lst_env_add_back(&head->env, new_env);
+		if (!head_env_init(envp[i], new_export, new_env))
 			return (0);
 		i++;
 	}
-	ascii_ordering(&head->export);
+	lst_env_sort(&head->export);
 	return (1);
 }
