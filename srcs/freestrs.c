04cbed66e64248d4b463df0a19ee4635c53e1fae
Maxence Liboz <maxoulib@gmail.com>
2022-01-12 17:12:04 +0100

    free a char **

--- srcs/freestrs.c
+++ srcs/freestrs.c
@@ -3,25 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   freestrs.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mliboz <mliboz@student.42.fr>              +#+  +:+       +#+        */
+/*   By: maxenceliboz <maxenceliboz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 10:33:11 by mliboz            #+#    #+#             */
-/*   Updated: 2022/01/07 10:35:07 by mliboz           ###   ########.fr       */
+/*   Updated: 2022/01/12 16:20:16 by maxencelibo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-char	**ft_free(char **str)
+void	ft_free(char **str)
 {
 	int		i;
+	int		y;
 
 	i = 0;
+	y = 0;
 	while (str[i])
 	{
-		freestr(str[i]);
+		while (str[i][y])
+		{
+			str[i][y] = 0;
+			y++;
+		}
 		i++;
 	}
 	free(str);
-	exit(-1);
 }
