2f9ea86289c71856119485b638bd2b3ae0025c4f
Maxence Liboz <maxoulib@gmail.com>
2022-01-25 16:38:36 +0100

    add malloc error message

--- srcs/malloc/malloc_functions.c
+++ srcs/malloc/malloc_functions.c
@@ -6,7 +6,7 @@
 /*   By: maxenceliboz <maxenceliboz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 08:44:01 by maxencelibo       #+#    #+#             */
-/*   Updated: 2022/01/19 14:00:53 by maxencelibo      ###   ########.fr       */
+/*   Updated: 2022/01/25 11:20:29 by maxencelibo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ void	*ft_malloc(t_list **mem, size_t size)
 void	ft_error_free(t_list **mem)
 {
 	ft_lstclear(mem, free);
+	printf("Malloc error occured\n");
 	exit(-1);
 }
