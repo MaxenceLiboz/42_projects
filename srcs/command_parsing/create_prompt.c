9e665ddf88506b10957e479ffddf40040e9b5450
mliboz <maxoulib@gmail.com>
2022-02-15 14:41:08 +0100

    Color make error with readline

--- srcs/command_parsing/create_prompt.c
+++ srcs/command_parsing/create_prompt.c
@@ -6,7 +6,7 @@
 /*   By: mliboz <mliboz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 13:35:52 by mliboz            #+#    #+#             */
-/*   Updated: 2022/02/05 13:55:27 by mliboz           ###   ########.fr       */
+/*   Updated: 2022/02/15 14:39:54 by mliboz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,7 @@ t_string	create_prompt(t_string pwd, t_list **mem)
 {
 	t_string	dst;
 
-	init_string(&dst, BLUE, TRUE, mem);
-	dup_string(&dst, pwd.str, dst.size - 1, mem);
-	dup_string(&dst, END_COLOR, dst.size - 1, mem);
-	dup_string(&dst, " 👉 ", dst.size - 1, mem);
+	init_string(&dst, pwd.str, TRUE, mem);
+	add_string(&dst, " > ", dst.size - 1, mem);
 	return (dst);
 }
