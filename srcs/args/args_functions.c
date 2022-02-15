3ab2c2df18848fc342231c71136828c8fc2de94f
mliboz <maxoulib@gmail.com>
2022-02-15 11:19:31 +0100

    Argument done

--- srcs/args/args_functions.c
+++ srcs/args/args_functions.c
@@ -6,207 +6,101 @@
 /*   By: mliboz <mliboz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 13:18:56 by maxencelibo       #+#    #+#             */
-/*   Updated: 2022/02/09 17:47:17 by mliboz           ###   ########.fr       */
+/*   Updated: 2022/02/15 11:10:09 by mliboz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-t_bool	is_arg(char *arg, int *start)
+/*
+	Get the whole argument returned as a string;
+*/
+t_string	get_full_arg(t_string *str, int start, t_list **mem)
 {
-	// printf("%s, %c, %c, %d\n", &arg[*start], arg[*start], arg[*start + 1], ft_isalnum(arg[*start + 1]) || arg[*start + 1] == '?');
-	if (arg[*start] == '$'
-		&& (ft_isalnum(arg[*start + 1]) || arg[*start + 1] == '?'))
-		return (TRUE);
-	else if (arg[*start] == '\"' && arg[*start + 1] == '$'
-		&& (ft_isalnum(arg[*start + 2]) || arg[*start + 2] == '?'))
+	int			end;
+	t_string	dst;
+
+	end = start + 1;
+	while (ft_isalnum(str->str[end]))
+		end++;
+	return (dst = sub_string(str->str, start, end - start, mem));
+}
+
+/*
+	Get only the argument => should be the same as the one in our list
+*/
+t_string	get_arg(t_string arg, t_list **mem)
+{
+	int			start;
+	int			end;
+	t_string	dst;
+
+	start = 1;
+	end = start;
+	while (ft_isalnum(arg.str[end]))
+		end++;
+	return (dst = sub_string(arg.str, start, end - start, mem));
+}
+
+void	find_arg_and_replace(t_prg *prg, int start, t_string *str)
+{
+	t_string	full_arg;
+	t_string	arg;
+	int			indexs[2];
+
+	full_arg = get_full_arg(str, start, &prg->mem);
+	arg = get_arg(full_arg, &prg->mem);
+	indexs[0] = start;
+	indexs[1] = full_arg.size - 1;
+	dup_string(&arg, lst_env_find_name_var(prg->env.export, arg.str).str,
+		0, &prg->mem);
+	if (ft_isdigit(full_arg.str[1]) == TRUE)
+		erase_string(str, "$n", start, &prg->mem);
+	else if (arg.str)
+		replace_string(str, indexs, arg.str, &prg->mem);
+	else
+		erase_string(str, full_arg.str, start, &prg->mem);
+}
+
+/*
+	Check if it is an arg and return TRUE or FALSE
+*/
+t_bool	is_arg(char *arg, int *index)
+{
+	if (arg[*index] == '$')
 	{
-		*start += 1;
-		return (TRUE);
+		if (ft_isalnum(arg[*index + 1]) || arg[*index + 1] == '?')
+			return (TRUE);
 	}
 	return (FALSE);
 }
 
-static void	next_arg(char *arg, int *start)
-{
-	if (arg[*start] == '<' || arg[*start] == '>')
-	{
-		while (arg[*start] != ' ' && arg[*start])
-			*start += 1;
-		while (arg[*start] == ' ' && arg[*start])
-			*start += 1;
-		while (arg[*start] != ' ' && arg[*start])
-			*start += 1;
-	}
-}
-
-void	find_arg_and_replace(t_prg *prg, t_string *arg, int start,
-	t_string *str)
-{
-	int			end;
-	t_string	tmp;
-	int			indexs[2];
-
-	end = start + 1;
-	if (str->str[start + 1] == '?')
-	{
-		init_string(arg, ft_itoa(prg->return_value), TRUE, &prg->mem);
-		indexs[0] = start;
-		indexs[1] = arg->size;
-		replace_string(str, indexs, arg->str, &prg->mem);
-		return ;
-	}
-	while (ft_isalnum(str->str[end]))
-		end++;
-	*arg = sub_string(str->str, start + 1, end - (start + 1),
-			&prg->mem);
-	tmp = lst_env_find_name_var(prg->env.export, arg->str);
-	indexs[0] = start;
-	indexs[1] = arg->size;
-	if (tmp.str)
-		replace_string(str, indexs, tmp.str, &prg->mem);
-	else
-		erase_string(str, arg->str, start, &prg->mem);
-}
-
+/*
+	Find envp varaible or $? and replace it by its value
+*/
 void	change_arg_command(t_prg *prg, t_string *str)
 {
-	int			start;
-	t_string	arg;
-	int			squotes;
+	int			index;
 	int			dquotes;
 
-	start = 0;
+	index = 0;
 	dquotes = -1;
-	squotes = -1;
-	init_string(&arg, "", TRUE, &prg->mem);
-	if (is_arg(str->str, &start))
-		find_arg_and_replace(prg, &arg, start, str);
-	start++;
-	while (str->str[start])
+	index++;
+	while (str->str[index])
 	{
-		if (str->str[start] == '\"' && squotes < 1)
+		if (str->str[index] == '\"')
 			dquotes *= -1;
-		if (str->str[start] == '\'' && dquotes < 1)
-			squotes *= -1;
-		if (squotes < 1 && dquotes < 1)
-			next_arg(str->str, &start);
-		if (is_arg(str->str, &start))
-			find_arg_and_replace(prg, &arg, start, str);
-		start++;
+		if (str->str[index] == '\'' && dquotes < 1)
+		{
+			while (str->str[++index] != '\'')
+				;
+			index++;
+		}
+		if (is_arg(str->str, &index) == TRUE)
+			find_arg_and_replace(prg, index, str);
+		if (dquotes < 1 && str->str[index] == '$' && str->str[index + 1]
+			&& (str->str[index + 1] == '\'' || str->str[index + 1] == '\"'))
+			erase_string(str, "$", index, &prg->mem);
+		index++;
 	}
 }
-
-
-
-// /*
-// 	Get the whole argument returned as a string;
-// */
-// t_string	get_full_arg(t_string *str, int start, t_list **mem)
-// {
-// 	int			end;
-// 	t_string	dst;
-
-// 	end = start + 1;
-// 	if (str->str[end] == "{")
-// 		end++;
-// 	while (ft_isalnum(str->str[end]))
-// 		end++;
-// 	if (str->str[end] == "}")
-// 		end++;
-// 	return (dst = sub_string(str->str, start, end - start, mem));
-// }
-
-// /*
-// 	Get only the argument => should be the same as the one in our list
-// */
-// t_string	get_arg(t_string arg, t_list **mem)
-// {
-// 	int			start;
-// 	int			end;
-// 	t_string	dst;
-
-// 	start = 0;
-// 	while (ft_isalnum(arg.str[start] == 1))
-// 		start++;
-// 	end = start;
-// 	while (ft_isalnum(arg.str[end]))
-// 		end++;
-// 	return (dst = sub_string(arg.str, start, end - start, mem));
-// }
-
-// void	find_arg_and_replace(t_prg *prg, int start, t_string *str)
-// {
-// 	t_string	full_arg;
-// 	t_string	arg;
-// 	int			indexs[2];
-
-// 	full_arg = get_full_arg(str, start, &prg->mem);
-// 	arg = get_arg(full_arg, &prg->mem);
-// 	dup_string(&arg, lst_env_find_name_var(prg->env.export, arg.str).str,
-// 		0, &prg->mem);
-// 	indexs[0] = start;
-// 	indexs[1] = full_arg.size;
-// 	if (arg.str)
-// 		replace_string(str, indexs, arg.str, &prg->mem);
-// 	else
-// 		erase_string(str, full_arg.str, start, &prg->mem);
-// }
-
-// /*
-// 	Remove $ sign if next to ' or "
-// */
-// void	remove_dolard_if_needed(t_string *str, int index, t_list **mem)
-// {
-// 	if (str->str[index] == '$' && str->str[index + 1]
-// 		&& (str->str[index + 1] == '\'' || str->str[index + 1] == '\"'))
-// 		erase_string(str, "$", index, mem);
-// }
-
-// /*
-// 	Check if it is an arg and return TRUE or FALSE
-// */
-// t_bool	is_arg(char *arg, int *index)
-// {
-// 	if (arg[*index] == '$')
-// 	{
-// 		if (ft_isalnum(arg[*index + 1]) || arg[*index + 1] == '?')
-// 			return (TRUE);
-// 		else if (arg[*index + 1] == '{' )
-// 		{
-// 			*index += 1;
-// 			return (TRUE);
-// 		}
-// 	}
-// 	return (FALSE);
-// }
-
-// /*
-// 	Find envp varaible and replace it by its value
-// */
-// void	change_arg_command(t_prg *prg, t_string *str)
-// {
-// 	int			index;
-// 	int			dquotes;
-
-// 	index = 0;
-// 	dquotes = -1;
-// 	if (is_arg(str->str, &index) == TRUE)
-// 		find_arg_and_replace(prg, index, str);
-// 	index++;
-// 	while (str->str[index])
-// 	{
-// 		if (str->str[index] == '\"')
-// 			dquotes *= -1;
-// 		if (str->str[index] == '\'' && dquotes > 1)
-// 		{
-// 			while (str->str[++index] != '\'')
-// 				;
-// 			index++;
-// 		}
-// 		if (is_arg(str->str, &index) == TRUE)
-// 			find_arg_and_replace(prg, index, str);
-// 		remove_dolard_if_needed(str, index, &prg->mem);
-// 		index++;
-// 	}
-// }
--- srcs/signal/set_term.c
+++ srcs/signal/set_term.c
@@ -6,7 +6,7 @@
 /*   By: mliboz <mliboz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 15:31:57 by tarchimb          #+#    #+#             */
-/*   Updated: 2022/02/09 16:13:35 by mliboz           ###   ########.fr       */
+/*   Updated: 2022/02/10 09:21:21 by mliboz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,11 @@
 void	set_term_env(t_list **mem)
 {
 	char	*tmp;
-	
-	(void)mem;
-	// if (tgetent(NULL, getenv("TERM")) == ERR)
-	// 	ft_error_exit(mem, 1, "getent: error");
-	tgetent(NULL, getenv("TERM"));
+
+	if (tgetent(NULL, getenv("TERM")) == ERR)
+		ft_error_exit(mem, 1, "getent: error");
 	tmp = tgetstr("cl", NULL);
-	// if (tmp == NULL)
-	// 	ft_error_exit(mem, 1, "tgetstr: error");
+	if (tmp == NULL)
+		ft_error_exit(mem, 1, "tgetstr: error");
 	tputs(tmp, 10, putchar);
 }
