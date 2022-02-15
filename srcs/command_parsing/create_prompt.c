5b4731e1be690c5ca935e8fa632db6e3214e1ef3
mliboz <maxoulib@gmail.com>
2022-02-15 15:02:49 +0100

    Error fix with color readline

--- srcs/command_parsing/create_prompt.c
+++ srcs/command_parsing/create_prompt.c
@@ -6,6 +6,7 @@
 /*   By: mliboz <mliboz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 13:35:52 by mliboz            #+#    #+#             */
+
 /*   Updated: 2022/02/15 14:39:54 by mliboz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -16,7 +17,10 @@ t_string	create_prompt(t_string pwd, t_list **mem)
 {
 	t_string	dst;
 
-	init_string(&dst, pwd.str, TRUE, mem);
-	add_string(&dst, " > ", dst.size - 1, mem);
+	init_string(&dst, BLUE, TRUE, mem);
+	add_string(&dst, pwd.str, dst.size - 1, mem);
+	add_string(&dst, YELLOW, dst.size - 1, mem);
+	add_string(&dst, " >> ", dst.size - 1, mem);
+	add_string(&dst, END_COLOR, dst.size - 1, mem);
 	return (dst);
 }
